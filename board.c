91feb7889b600646f4edde4f8854cfc0a9250a79
tomas-20 <tacuna20@stuy.edu>
2022-01-19 11:48:12 -0500

    tried to make a winner checker thing but it doesn't work yet

--- board.c
+++ board.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
+int real_row(int r) {
+  return r >= 0 && r < 6;
+}
+
+int real_col(int c) {
+  return c >= 0 && c < 7;
+}
+
+
 void clear_board(char board[6][7]) {
-  for (int r = 0; r < 6; r ++) {
-    for (int c = 0; c < 7; c ++) {
+  for (int r = 0; real_row(r); r ++) {
+    for (int c = 0; real_col(c); c ++) {
       board[r][c] = 0;
     }
   }
 }
 
 int place_piece(char board[6][7], char player, int c) {
-  for (int r = 5; r >= 0; r --) {
+  for (int r = 5; real_row(r); r --) {
     if (board[r][c] == 0) {
       board[r][c] = player;
       return 0;
@@ -19,7 +28,7 @@ int place_piece(char board[6][7], char player, int c) {
 }
 
 int full(char board[6][7]) {
-  for (int c = 0; c < 7; c ++) {
+  for (int c = 0; real_col(c); c ++) {
     if (board[0][c] == 0) {
       return 0;
     }
@@ -41,10 +50,48 @@ void print_hole(char player) {
 
 void print_board(char board[6][7]) {
   printf("1 2 3 4 5 6 7 \n");
-  for (int r = 0; r < 6; r ++) {
-    for (int c = 0; c < 7; c ++) {
+  for (int r = 0; real_row(r); r ++) {
+    for (int c = 0; real_col(c); c ++) {
       print_hole(board[r][c]);
     }
     printf("\n");
   }
+}
+
+int check_hole(char board[6][7], char *last, int *consec, int r, int c) {
+  if (board[r][c] == *last) {
+    (*consec) ++;
+    if (*consec >= 4) {
+      return 1;
+    }
+  }
+  else {
+    *last = board[r][c];
+    *consec = 1;
+  }
+  return 0;
+}
+
+char get_winner(char board[6][7]) {
+  char last;
+  int consec;
+  for (int r = 0; real_row(r); r ++) {
+    last = 0;
+    consec = 0;
+    for (int c = 0; real_col(c); c ++) {
+      if (check_hole(board, &last, &consec, r, c)) {
+        return last;
+      }
+    }
+  }
+  for (int c = 0; real_col(c); c ++) {
+    last = 0;
+    consec = 0;
+    for (int r = 0; real_row(r); r ++) {
+      if (check_hole(board, &last, &consec, r, c)) {
+        return last;
+      }
+    }
+  }
+  return 0;
 }
---
+++ old_game.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "board.h"
+
+int get_int(char player) {
+  char input[100];
+  fgets(input, sizeof input, stdin);
+  int num = atoi(input);
+  while (num < 1 || num > 7){
+    printf("Not a valid input. Try again\n");
+    if (player == 1) {
+      printf("place red piece on: ");
+    }
+    else {
+      printf("place blue piece on: ");
+    }
+    fgets(input, sizeof input, stdin);
+    num = atoi(input);
+  }
+  return atoi(input);
+}
+
+int do_turn_attempt(char board[6][7], char player) {
+  if (player == 1) {
+    printf("place red piece on: ");
+  }
+  else {
+    printf("place blue piece on: ");
+  }
+  int c = get_int(player);
+  int fail = place_piece(board, player, c - 1);
+  if (fail) {
+    printf("slot %c is full\n", c);
+  }
+  return fail;
+}
+
+void do_turn(char board[6][7], char player) {
+  while (do_turn_attempt(board, player)) {}
+}
+
+void show_result(char winner) {
+  if (winner == 0) {
+    printf("The game is a tie!");
+  }
+  else if (winner == 1) {
+    printf("Player 1 wins!")'
+  }
+  else if (winnner == 2) {
+    printf("Player 2 wins!");
+  }
+}
+
+void play_game() {
+  char board[6][7];
+  clear_board(board);
+  char player = 1;
+  char winner = 0;
+  while (!full(board) && !winner) {
+    print_board(board);
+    do_turn(board, player);
+    winner = get_winner(board);
+    if (player == 1) {
+      player = 2;
+    }
+    else {
+      player = 1;
+    }
+  }
+}
+
+int main() {
+  play_game();
+  return 0;
+}
